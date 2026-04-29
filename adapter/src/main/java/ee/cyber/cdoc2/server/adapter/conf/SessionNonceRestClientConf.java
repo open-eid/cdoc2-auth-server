2c8b2c6c9ba4388cc7f8fd5787e37456589a7ed4
Anti Ramst <anti.ramst@cyber.ee>
2026-04-29 17:13:32 +0300

    CDOC-154: use https, provide default configuration

--- adapter/src/main/java/ee/cyber/cdoc2/server/adapter/conf/SessionNonceRestClientConf.java
+++ adapter/src/main/java/ee/cyber/cdoc2/server/adapter/conf/SessionNonceRestClientConf.java
@@ -1,12 +1,29 @@
 package ee.cyber.cdoc2.server.adapter.conf;
 
+import lombok.Getter;
+import java.security.KeyStore;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.ClientTlsStrategyBuilder;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.http.ssl.TLS;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
+@Getter
 @Configuration
 public class SessionNonceRestClientConf {
     private static final String CONF_DEFAULT_READ_TIMEOUT = "5000";
@@ -15,8 +32,12 @@ public class SessionNonceRestClientConf {
 
     private final int retries;
 
-    public SessionNonceRestClientConf(AppProperties props) {
+    private static final String SSL_BUNDLE_NAME = "session-nonce-server";
+    private final SslBundles sslBundles;
+
+    public SessionNonceRestClientConf(AppProperties props, SslBundles sslBundles) {
         retries = props.retries;
+        this.sslBundles = sslBundles;
     }
 
     @ConfigurationProperties(prefix = "app.restclient.session-nonce")
@@ -28,18 +49,39 @@ public class SessionNonceRestClientConf {
     }
 
     @Bean
-    public RestClient sessionNonceRestClient(AppProperties props) {
-        HttpComponentsClientHttpRequestFactory factory =
-            new HttpComponentsClientHttpRequestFactory();
-        factory.setReadTimeout(props.readTimeout);
-        factory.setConnectionRequestTimeout(props.connectionRequestTimeout);
+    public RestClient sessionNonceRestClient(AppProperties props) throws Exception {
+        KeyStore trustStore = sslBundles.getBundle(SSL_BUNDLE_NAME).getStores().getTrustStore();
+
+        SSLContext sslContext = SSLContexts.custom()
+            .loadTrustMaterial(trustStore, null)
+            .build();
+
+        PoolingHttpClientConnectionManager connectionManager =
+            PoolingHttpClientConnectionManagerBuilder.create()
+                .setTlsSocketStrategy(
+                    ClientTlsStrategyBuilder.create()
+                        .setSslContext(sslContext)
+                        .setTlsVersions(TLS.V_1_3)
+                        .buildClassic()
+                )
+                .setDefaultSocketConfig(
+                    SocketConfig.custom()
+                        .setSoTimeout(Timeout.ofMilliseconds(props.readTimeout()))
+                        .build()
+                )
+                .setDefaultConnectionConfig(
+                    ConnectionConfig.custom()
+                        .setConnectTimeout(Timeout.ofMilliseconds(props.connectionRequestTimeout()))
+                        .build()
+                )
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+            .setConnectionManager(connectionManager)
+            .build();
 
         return RestClient.builder()
-            .requestFactory(factory)
+            .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
             .build();
-    }
-
-    public int getRetries() {
-        return retries;
     }
 }
