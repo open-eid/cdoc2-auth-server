b01d6a9d4c1b0b387883f1832e7828148037e4ef
Anti Ramst <anti.ramst@cyber.ee>
2026-05-12 15:38:05 +0300

    CDOC-160: cleanup

--- app/src/main/java/ee/cyber/cdoc2/server/app/usecase/startauth/StartAuthImpl.java
+++ app/src/main/java/ee/cyber/cdoc2/server/app/usecase/startauth/StartAuthImpl.java
@@ -15,7 +15,6 @@ import org.springframework.stereotype.Component;
 public class StartAuthImpl implements StartAuth {
     private static final int RP_CHALLENGE_BYTES_LENGTH = 64;
 
-    private final SessionNonce sessionNonce;
     private final StartSidAuth startSidAuth;
     private final StartMidAuth startMidAuth;
 
@@ -37,65 +36,9 @@ public class StartAuthImpl implements StartAuth {
         );
     }
 
-//    private String getAndValidatePhoneNumber(Request request) {
-//        try {
-//            Objects.requireNonNull(request.mobileNr());
-//            return MidInputUtil.getValidatedPhoneNumber(request.mobileNr());
-//        } catch (MidInvalidPhoneNumberException e) {
-//            throw new InputValidationException(e.getMessage(), e);
-//        }
-//    }
-//
-//    private String getAndValidateNationalIdentityNumber(Request request) {
-//        try {
-//            return MidInputUtil.getValidatedNationalIdentityNumber(request.nationalId());
-//        } catch (MidInvalidNationalIdentityNumberException e) {
-//            throw new InputValidationException(e.getMessage(), e);
-//        }
-//    }
-
     private static byte[] createRpChallengeBytes() {
         byte[] rpChallengeBytes = new byte[RP_CHALLENGE_BYTES_LENGTH];
         new SecureRandom().nextBytes(rpChallengeBytes);
         return rpChallengeBytes;
     }
-
-//    private Response performMidAuth(Request request) {
-//        String validPhoneNumber = getAndValidatePhoneNumber(request);
-//        String validNationalIdentityNumber = getAndValidateNationalIdentityNumber(request);
-//
-//        List<UriSessionNonce> sessionNonces = sessionNonce.collectSessionNonces();
-//
-//        SessionTokenCreationParams tokenCreationParams = new SessionTokenCreationParams(
-//            sessionNonces,
-//            validNationalIdentityNumber,
-//            "https://cdoc2-auth-server.ee"
-//        );
-//
-//        SDJWT unsignedSdJWT = SessionToken.unsignedSdJwtWithAllDisclosures(tokenCreationParams);
-//
-//        byte[] rpChallenge = createRpChallengeBytes();
-//
-//        MidHashToSign hashToSign = MidHashToSign.newBuilder()
-//            .withDataToHash(rpChallenge)
-//            .withHashType(MidHashType.SHA256)
-//            .build();
-//
-//        byte[] hashBytes = hashToSign.getHash();
-//
-//        MidAuthenticationHashToSign authenticationHash = MidAuthenticationHashToSign.newBuilder()
-//            .withHash(hashBytes)
-//            .withHashType(MidHashType.SHA256)
-//            .build();
-//
-//        UUID sessionId = midAuthenticate.execute(new MidAuthenticate.Request(
-//                validPhoneNumber,
-//                validNationalIdentityNumber,
-//                authenticationHash
-//            )
-//        );
-//        String verificationCode = authenticationHash.calculateVerificationCode();
-//
-//        return new Response(sessionId, verificationCode);
-//    }
 }
