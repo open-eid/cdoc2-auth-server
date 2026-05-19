663ce5010ec5fd05891178a50700d4e10fdfb4f7
Anti Ramst <anti.ramst@cyber.ee>
2026-05-19 16:03:51 +0300

    CDOC-150: use correct SSL bundle for MID cert validation

--- app/src/main/java/ee/cyber/cdoc2/server/app/conf/MidConf.java
+++ app/src/main/java/ee/cyber/cdoc2/server/app/conf/MidConf.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MidConf {
-    private static final String SSL_BUNDLE_NAME = "mid-server";
+    private static final String SSL_BUNDLE_NAME = "sid-trusted-issuers";
 
     @Bean
     public MidAuthenticationResponseValidator midAuthenticationResponseValidator(
