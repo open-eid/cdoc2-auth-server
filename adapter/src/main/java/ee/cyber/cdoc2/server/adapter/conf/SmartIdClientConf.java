6e81a84b7bb856387162801be6182195f8dd0342
Anti Ramst <anti.ramst@cyber.ee>
2026-08-15 11:03:43 +0300

    CDOC-222: changed parameter name slightly, updated readme, changelog

--- adapter/src/main/java/ee/cyber/cdoc2/server/adapter/conf/SmartIdClientConf.java
+++ adapter/src/main/java/ee/cyber/cdoc2/server/adapter/conf/SmartIdClientConf.java
@@ -16,14 +16,14 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class SmartIdClientConf {
     private static final String SSL_BUNDLE_NAME = "sid-server";
-    private static final String DEFAULT_TIMEOUT_SECONDS = "1";
+    private static final String DEFAULT_STATUS_POLL_TIMEOUT_SECONDS = "1";
 
     private final SslBundles sslBundles;
 
     @ConfigurationProperties(prefix = "app.smartid.client")
     public record AppProperties(
         String hostUrl,
-        @DefaultValue(DEFAULT_TIMEOUT_SECONDS) long timeoutSeconds
+        @DefaultValue(DEFAULT_STATUS_POLL_TIMEOUT_SECONDS) long statusPollTimeoutSeconds
     ) {
     }
 
@@ -35,7 +35,7 @@ public class SmartIdClientConf {
         smartIdClient.setHostUrl(props.hostUrl);
         smartIdClient.setTrustStore(trustStore);
         smartIdClient.setSessionStatusResponseSocketOpenTime(
-            TimeUnit.SECONDS, props.timeoutSeconds
+            TimeUnit.SECONDS, props.statusPollTimeoutSeconds
         );
 
         return smartIdClient;
